3ad8db0eddd1842b319e08c9f20d474e41368a60
toneswari <toneswarinandam037@gmail.com>
2024-06-27 19:48:53 +0530

    changes for the delete button
    delete

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,11 +1,14 @@
 import styles from "./Todoitem.module.css";
 export default function Todoitem({item}){
+    function deleteTodo(){
+        console.log("deleting tod");
+    }
     return(
         <div className={styles.items}>
 <div className={styles.itemName}>
     {item}
     <span>
-        <button className={styles.mybutton}>x</button>
+        <button className={styles.mybutton} onClick={deleteTodo}>x</button>
     </span>
 </div>
 <hr className={styles.line}/>
